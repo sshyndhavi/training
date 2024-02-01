43e36f1377a9eef97819c6d008032751e8c4f894
sshyndhavi <ssaihyndhavi@gmail.com>
2024-02-01 23:14:01 +0530

    commit params method

---
+++ Coding/params method/params method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Calling the method with different numbers of arguments
+        DisplayNumbers(1, 2, 3);
+        DisplayNumbers(10, 20, 30, 40, 50);
+        DisplayNumbers(); // No arguments
+    }
+
+    // Method that takes a variable number of integer arguments using 'params'
+    static void DisplayNumbers(params int[] numbers)
+    {
+        Console.WriteLine("The numbers are:");
+
+        // Iterate through the array of numbers and display each one
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        Console.WriteLine();
+    }
+}
