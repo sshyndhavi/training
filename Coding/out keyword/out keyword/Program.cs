ffcbff23c582fc0b48e756d4bc8ce5e5167e425f
sshyndhavi <ssaihyndhavi@gmail.com>
2024-02-01 23:10:03 +0530

    commit out keyword

---
+++ Coding/out keyword/out keyword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int result; // Variable to store the output parameter
+
+        // Call the method with the 'out' parameter
+        Add(5, 3, out result);
+
+        // Print the result
+        Console.WriteLine("The sum is: " + result); // Output will be 8
+    }
+
+    // Method that calculates the sum of two numbers and returns it through an 'out' parameter
+    static void Add(int a, int b, out int sum)
+    {
+        sum = a + b; // The result is assigned to the 'out' parameter
+    }
+}
