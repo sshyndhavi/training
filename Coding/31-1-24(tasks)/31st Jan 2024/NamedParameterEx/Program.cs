aee39bd1db9be86f6c82c522facd8d11d91dcae2
sshyndhavi <ssaihyndhavi@gmail.com>
2024-02-01 15:59:39 +0530

    commit named parameters

---
+++ Coding/31-1-24(tasks)/31st Jan 2024/NamedParameterEx/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameterEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int areaResult = CalculateArea(breadth: 20, length: 10);
+            Console.WriteLine("Length {0} * Breadth {1} = Area {2}", 10, 20, areaResult);
+            Console.Read();
+
+        }
+        public static int CalculateArea(int length, int breadth)
+        {
+            return length * breadth;
+        }
+
+    }
+}
