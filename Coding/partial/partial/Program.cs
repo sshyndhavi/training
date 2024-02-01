440808503dee0044a2e6b5f8c863238af9fe9b3f
sshyndhavi <ssaihyndhavi@gmail.com>
2024-02-01 15:57:40 +0530

    commit partial file

---
+++ Coding/partial/partial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+ partial class MyClass
+{
+    partial void DisplayMessage(string message);
+
+     public void CallDisplayMessage()
+    {
+        DisplayMessage("Hello, world!"); // This method call may or may not be implemented
+    }
+}
+
+partial class MyClass
+{
+    partial void DisplayMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+        myObject.CallDisplayMessage(); // This will display "Hello, world!" on the console
+    }
+}
+
+
