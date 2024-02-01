e7f3de0b407e05af6e2f571853423650595996d0
sshyndhavi <ssaihyndhavi@gmail.com>
2024-02-01 16:15:11 +0530

    commit async

---
+++ Coding/async_await/async_await/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+class program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("start");
+        await performasync();
+        Console.WriteLine("end");
+    }
+    static async Task performasync()
+    {
+        Console.WriteLine("inside performasync");
+        await Task.Delay(1000);
+        Console.WriteLine("async operation completed");
+    }
+}
